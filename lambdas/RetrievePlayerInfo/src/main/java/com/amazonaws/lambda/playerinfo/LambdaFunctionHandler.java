a6da85411006b649e0c546ada08816412e1627ba
s3656024 <s3656024@student.rmit.edu.au>
2021-01-29 15:09:17 +1100

    hoping this fixes something

--- lambdas/RetrievePlayerInfo/src/main/java/com/amazonaws/lambda/playerinfo/LambdaFunctionHandler.java
+++ lambdas/RetrievePlayerInfo/src/main/java/com/amazonaws/lambda/playerinfo/LambdaFunctionHandler.java
@@ -20,48 +20,51 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 
-public class LambdaFunctionHandler implements RequestHandler<Map<String,String>, List<PlayerInfo>> {
-  	static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-  	static DynamoDB dynamoDB = new DynamoDB(client);
+public class LambdaFunctionHandler implements RequestHandler<Map<String,Object>, Map<String, Object>> {
+	static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+	static DynamoDB dynamoDB = new DynamoDB(client);
 	static DynamoDBMapper mapper = new DynamoDBMapper(client);
 
-    public LambdaFunctionHandler() {}
+	public LambdaFunctionHandler() {}
 
-    public List<PlayerInfo> handleRequest(Map<String,String> event, Context context) {
-        context.getLogger().log("Received event: " + event);
-        Table userInformation = dynamoDB.getTable("user_details");
+	public Map<String, Object> handleRequest(Map<String,Object> event, Context context) {
+//		Gson g = new Gson();  
+//		JsonReader reader = new JsonReader(new StringReader(event.toString()));
+//		reader.setLenient(true);
+//
+//		JsonObject json = g.fromJson(reader, JsonObject.class);
+
+		context.getLogger().log("Received event: " + event);
+
+		Table userInformation = dynamoDB.getTable("user_details");
 
 		List<PlayerInfo> scanResult = mapper.scan(PlayerInfo.class, new DynamoDBScanExpression());
 		List<PlayerInfo> items = new ArrayList<>();		
-		
-//        Gson g = new Gson();  
-//        JsonReader reader = new JsonReader(new StringReader(event.toString()));
-//        reader.setLenient(true);
-//        
-//        JsonObject json = g.fromJson(reader, JsonObject.class);
-        String username = event.get("username");
 
-        for(PlayerInfo currentPlayer: scanResult) {
-        	if (currentPlayer.getUserName().equals(username)) {
-        			
-        		List<String> players = Arrays.asList(currentPlayer.getPlayers().split(","));
-        		for(String currentPlayedPlayer: players) {
-        			
-        			GetItemSpec playerInformation = new GetItemSpec().withPrimaryKey("username", currentPlayedPlayer);
-        			currentPlayer.addPlayerInfo(userInformation.getItem(playerInformation).asMap());
-        		}
-        		
-        		
-        		items.add(currentPlayer);
-        	}
-        }
-        
-        Map<String, Object> response = new HashMap<>();
-        Map<String, String> headers = new HashMap<>();
-//        response.put("statusCode", 200);
-        headers.put("Access-Control-Allow-Origin", "*");
-        response.put("headers", headers);
-        response.put("body", items);
-        return items;
-    }
+		String username = "Darknight091";
+
+		for(PlayerInfo currentPlayer: scanResult) {
+			if (currentPlayer.getUserName().equals(username)) {
+
+				List<String> players = Arrays.asList(currentPlayer.getPlayers().split(","));
+				for(String currentPlayedPlayer: players) {
+
+					GetItemSpec playerInformation = new GetItemSpec().withPrimaryKey("username", currentPlayedPlayer);
+					currentPlayer.addPlayerInfo(userInformation.getItem(playerInformation).asMap());
+				}
+
+
+				items.add(currentPlayer);
+			}
+		}
+
+		Map<String, Object> response = new HashMap<>();
+		Map<String, String> headers = new HashMap<>();
+		response.put("statusCode", 200);
+		headers.put("Access-Control-Allow-Origin", "*");
+		response.put("isBase64Encoded", false);
+		response.put("headers", headers);
+		response.put("body", "hi");
+		return response;
+	}
 }
